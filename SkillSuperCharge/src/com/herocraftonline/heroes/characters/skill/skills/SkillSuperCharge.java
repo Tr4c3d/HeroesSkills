01b7d8f4bf9539217b974c80f27c70ed41b717a3
Alexandre Bueno Bacellar <whatshiywl@hotmail.com>
2012-09-24 17:25:58 -0300

    Changes on SuperCharge
    The skill now directly shoots an over powered arrow. You can set it's
    speed multiplier. The skill requires a bow in the inventory to be used
    and setting an arrow as reagent is suggested.

--- SkillSuperCharge/src/com/herocraftonline/heroes/characters/skill/skills/SkillSuperCharge.java
+++ SkillSuperCharge/src/com/herocraftonline/heroes/characters/skill/skills/SkillSuperCharge.java
@@ -2,87 +2,57 @@ package com.herocraftonline.heroes.characters.skill.skills;
 
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.SkillResult;
-import com.herocraftonline.heroes.api.events.WeaponDamageEvent;
 import com.herocraftonline.heroes.characters.Hero;
-import com.herocraftonline.heroes.characters.effects.EffectType;
-import com.herocraftonline.heroes.characters.effects.common.ImbueEffect;
 import com.herocraftonline.heroes.characters.skill.ActiveSkill;
-import com.herocraftonline.heroes.characters.skill.Skill;
 import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
 import com.herocraftonline.heroes.characters.skill.SkillType;
-import com.herocraftonline.heroes.util.Setting;
-import org.bukkit.Bukkit;
+import com.herocraftonline.heroes.util.Messaging;
+
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
+import org.bukkit.entity.Arrow;
 
 public class SkillSuperCharge extends ActiveSkill
 {
-  public SkillSuperCharge(Heroes plugin)
-  {
-    super(plugin, "SuperCharge");
-    setDescription("Shoots an extra charged arrow");
-    setUsage("/skill SuperCharge");
-    setArgumentRange(0, 0);
-    setIdentifiers(new String[] { "skill SuperCharge" });
-    setTypes(new SkillType[] { SkillType.HARMFUL, SkillType.BUFF });
-
-    Bukkit.getServer().getPluginManager().registerEvents(new SkillEntityListener(this), plugin);
-  }
-
-    @Override
-  public ConfigurationSection getDefaultConfig()
-  {
-    ConfigurationSection node = super.getDefaultConfig();
-    node.set(Setting.DAMAGE.node(), 5);
-    node.set(Setting.DAMAGE_INCREASE.node(), 0);
-    return node;
-  }
-
-    @Override
-  public SkillResult use(Hero hero, String[] args)
-  {
-    hero.addEffect(new SuperChargeBuff(this));
-    broadcastExecuteText(hero);
-    return SkillResult.NORMAL;
-  }
-
-    @Override
-  public String getDescription(Hero hero)
-  {
-    return getDescription();
-  }
-
-  public class SuperChargeBuff extends ImbueEffect
-  {
-    public SuperChargeBuff(Skill skill)
+	public SkillSuperCharge(Heroes plugin)
+	{
+		super(plugin, "SuperCharge");
+		setDescription("Shoots an extra charged arrow");
+		setUsage("/skill SuperCharge");
+		setArgumentRange(0, 0);
+		setIdentifiers(new String[] { "skill SuperCharge" });
+		setTypes(new SkillType[] { SkillType.HARMFUL });
+	}
+	
+	@Override
+    public String getDescription(Hero hero)
     {
-      super(skill, "SuperChargeBuff");
-      this.types.add(EffectType.HARMFUL);
-      setDescription("Super Charge!");
-    }
-  }
-
-  public class SkillEntityListener implements Listener {
-    private final Skill skill;
-
-    public SkillEntityListener(Skill skill) {
-      this.skill = skill;
+    	return getDescription();
     }
 
-    @EventHandler
-    public void onEntityDamage(WeaponDamageEvent event) {
-
-      if(!(event.isCancelled())&&(event.getDamager() instanceof Hero)){
-    	  Hero hero = (Hero) event.getDamager();
-    	  if (hero.hasEffect("SuperChargeBuff")) {
-    		  int damage = (int) (SkillConfigManager.getUseSetting(hero, this.skill, Setting.DAMAGE.node(), 5.0, false) +
-    	              (SkillConfigManager.getUseSetting(hero, this.skill, Setting.DAMAGE_INCREASE.node(), 0.0, false) * hero.getSkillLevel(this.skill)));
-    	      damage = damage > 0 ? damage : 0;
-    		  event.setDamage(damage);
-    		  hero.removeEffect(hero.getEffect("SuperChargeBuff"));
-    	  }
-      }
+    @Override
+    public ConfigurationSection getDefaultConfig()
+    {
+    	ConfigurationSection node = super.getDefaultConfig();
+    	node.set("multiplier", 2.0);
+    	node.set("multiplier-increase", 0.0);
+    	return node;
     }
-  }
+
+    @Override
+    public SkillResult use(Hero hero, String[] args)
+    {
+    	if(!hero.getPlayer().getInventory().contains(261)){
+    		Messaging.send(hero.getPlayer(), "Must have a bow on inventory to use this skill!");
+    		return SkillResult.CANCELLED;
+    	}
+    	else{
+    		broadcastExecuteText(hero);
+    		float multiplier = (float) (SkillConfigManager.getUseSetting(hero, this, "multiplier", 2.0, false) +
+    				(SkillConfigManager.getUseSetting(hero, this, "multiplier-increase", 0.0, false) * hero.getSkillLevel(this)));
+    		multiplier = multiplier > 0 ? multiplier : 0;
+    		Arrow arrow = hero.getPlayer().launchProjectile(Arrow.class);
+    		arrow.setVelocity(arrow.getVelocity().normalize().multiply(multiplier));
+    		return SkillResult.NORMAL;
+    	}
+    }
 }
