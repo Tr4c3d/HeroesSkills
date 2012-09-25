dcddc05af712fe0b3046f2c00c19d1504513804a
Alexandre Bueno Bacellar <whatshiywl@hotmail.com>
2012-09-25 19:39:37 -0300

    Improved SuperCharge
    It now shoots direcrlty from the bow, becoming an effect skill. Also
    fixed the multiplier making no sense. It now shoots an arrow excactly
    the way multiplier tell it to.

--- Skills/SkillSuperCharge.java
+++ Skills/SkillSuperCharge.java
@@ -3,56 +3,84 @@ package com.herocraftonline.heroes.characters.skill.skills;
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.SkillResult;
 import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.effects.common.ImbueEffect;
 import com.herocraftonline.heroes.characters.skill.ActiveSkill;
+import com.herocraftonline.heroes.characters.skill.Skill;
 import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
 import com.herocraftonline.heroes.characters.skill.SkillType;
-import com.herocraftonline.heroes.util.Messaging;
-
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.Arrow;
+import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityShootBowEvent;
 
 public class SkillSuperCharge extends ActiveSkill
 {
 	public SkillSuperCharge(Heroes plugin)
 	{
 		super(plugin, "SuperCharge");
-		setDescription("Shoots an extra charged arrow");
+		setDescription("Shoots an extra-charged arrow");
 		setUsage("/skill SuperCharge");
 		setArgumentRange(0, 0);
 		setIdentifiers(new String[] { "skill SuperCharge" });
-		setTypes(new SkillType[] { SkillType.HARMFUL });
+		setTypes(new SkillType[] { SkillType.HARMFUL, SkillType.BUFF });
+
+		Bukkit.getServer().getPluginManager().registerEvents(new SkillEntityListener(this), plugin);
 	}
-	
+
 	@Override
-    public String getDescription(Hero hero)
-    {
-    	return getDescription();
-    }
+	public ConfigurationSection getDefaultConfig()
+	{
+		ConfigurationSection node = super.getDefaultConfig();
+		node.set("multiplier", 2.0);
+		node.set("multiplier-increase", 0.0);
+		return node;
+	}
 
-    @Override
-    public ConfigurationSection getDefaultConfig()
-    {
-    	ConfigurationSection node = super.getDefaultConfig();
-    	node.set("multiplier", 2.0);
-    	node.set("multiplier-increase", 0.0);
-    	return node;
-    }
+	@Override
+	public SkillResult use(Hero hero, String[] args)
+	{
+		hero.addEffect(new SuperChargeBuff(this));
+		broadcastExecuteText(hero);
+		return SkillResult.NORMAL;
+	}
 
-    @Override
-    public SkillResult use(Hero hero, String[] args)
-    {
-    	if(!hero.getPlayer().getInventory().contains(261)){
-    		Messaging.send(hero.getPlayer(), "Must have a bow on inventory to use this skill!");
-    		return SkillResult.CANCELLED;
-    	}
-    	else{
-    		broadcastExecuteText(hero);
-    		float multiplier = (float) (SkillConfigManager.getUseSetting(hero, this, "multiplier", 2.0, false) +
-    				(SkillConfigManager.getUseSetting(hero, this, "multiplier-increase", 0.0, false) * hero.getSkillLevel(this)));
-    		multiplier = multiplier > 0 ? multiplier : 0;
-    		Arrow arrow = hero.getPlayer().launchProjectile(Arrow.class);
-    		arrow.setVelocity(arrow.getVelocity().normalize().multiply(multiplier));
-    		return SkillResult.NORMAL;
-    	}
-    }
+	@Override
+	public String getDescription(Hero hero)
+	{
+		return getDescription();
+	}
+
+	public class SuperChargeBuff extends ImbueEffect
+	{
+		public SuperChargeBuff(Skill skill)
+		{
+			super(skill, "SuperChargeBuff");
+			setDescription("ChargedArrow");
+		}
+	}
+
+	public class SkillEntityListener implements Listener {
+		private final Skill skill;
+		public SkillEntityListener(Skill skill) {
+			this.skill = skill;
+		}
+
+		@EventHandler(priority=EventPriority.MONITOR)
+		public void onEntityShootBow(EntityShootBowEvent event) {
+			if ((event.isCancelled()) || (!(event.getEntity() instanceof Player)) || (!(event.getProjectile() instanceof Arrow))) {
+				return;
+			}
+			Hero hero = SkillSuperCharge.this.plugin.getCharacterManager().getHero((Player)event.getEntity());
+			if (hero.hasEffect("SuperChargeBuff")) {        
+				float multiplier = (float) (SkillConfigManager.getUseSetting(hero, this.skill, "multiplier", 2.0, false) +
+						(SkillConfigManager.getUseSetting(hero, this.skill, "multiplier-increase", 0.0, false) * hero.getSkillLevel(this.skill)));
+				multiplier = multiplier > 0 ? multiplier : 0;
+				event.getProjectile().setVelocity(event.getProjectile().getVelocity().multiply(multiplier));
+				hero.removeEffect(hero.getEffect("SuperChargeBuff"));
+			}
+		}
+	}
 }
